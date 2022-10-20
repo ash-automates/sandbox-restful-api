a4d8174ef49cc5467c5b44e3361a164b4d042eef
shachem <sekki.hachem@outlook.com>
2022-10-20 11:33:56 +0100

    Respond to requests from "/books" with success msg

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const app = express();
 app.listen(8000, () => {
   console.log("Success: listening for requests on port 8000...");
 });
+
+app.get("/books", (req, res) => {
+  res.json({ message: "response successful from /books" });
+});

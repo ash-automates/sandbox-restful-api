20f729c0398bd6c148ebbd23d7077b89a294afa7
shachem <sekki.hachem@outlook.com>
2022-10-21 09:56:32 +0100

    Set up functions to handle app's DB connection

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const { connectToDB, getDB } = require("./db");
+require("dotenv").config();
 
 const app = express();
-app.listen(8000, () => {
-  console.log("Success: listening for requests on port 8000...");
+
+let database;
+connectToDB((error) => {
+  if (!error) {
+    database = getDB();
+    app.listen(8000, () => {
+      console.log("Success: listening for requests on port 8000...");
+    });
+  }
 });
 
 app.get("/books", (req, res) => {
--- db.js
+++ db.js
@@ -1,1 +1,19 @@
-require("dotenv").config();
+const { MongoClient } = require("mongodb");
+let connection;
+
+module.exports = {
+  connectToDB: (callback) => {
+    MongoClient.connect(process.env.DB_URI)
+      .then((client) => {
+        connection = client.db();
+        return callback();
+      })
+      .catch((error) => {
+        console.log(error);
+        return callback(error);
+      });
+  },
+  getDB: () => {
+    return connection;
+  },
+};
